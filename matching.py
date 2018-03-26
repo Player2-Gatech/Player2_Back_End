cacf894bc2eb1f751e57515208e92ce5e3155b15
Matt Gordon <coltfanman@gmail.com>
2018-03-26 04:44:04 -0400

    matching algorithm, untested with db entries

--- matching.py
+++ matching.py
@@ -1,15 +1,73 @@
 import random
 
-def sort_matches(target_player, elligible_players):
+lol_tiers = {
+        'unranked': 0,
+        'bronze': 2,
+        'silver': 3,
+        'gold': 4,
+        'platinum': 5,
+        'diamond': 6,
+        'master': 7,
+        'challenger': 8
+        }
 
-     scored_players = map(lambda p: compute_similarity(target_player, p), elligible_players)
-     scored_players.sort(key = lambda p: p['score'], reverse = True)
-     return scored_players
+def sort_matches(target_player, elligible_players):
+    scored_players = map(lambda p: compute_similarity(target_player, p), elligible_players)
+    #filter out inelligible players
+    scored_players = [x for x in scored_players if x > 0]
+    scored_players.sort(key = lambda p: p['score'], reverse = True)
+    return scored_players
 
 # player_other will have an additional field called 'score' post method-call
 def compute_similarity(player_target, player_other):
-    player_other['score'] = random.uniform(0, 1)
+    score = 0
+    skill_modifier = .7
+    role_modifier = .299
+    like_modifier = .001
+
+    target_tier = lol_tiers[player_target['playerSkill']['tier'].lower()] 
+    other_tier = lol_tiers[player_other['playerSkill']['tier'].lower()] 
+    tier_diff = abs(target_tier - other_tier)
+
+    target_rank = player_target['playerSkill']['rank']
+    other_rank = player_target['playerSkill']['rank']
+
+    #filter out players with the same role
+    if player_target['playerGameRole']['role'] == player_other['playerGameRole']['role']:
+        player_other['score'] = 0
+        return player_other
+
+    #filter out players who are unable to queue together
+    if target_tier < 5:
+        if tier_diff > 1:
+            player_other['score'] = 0
+            return player_other
+    if target_tier > 4:
+        if skill_diff(target_tier, target_rank, other_tier, other_rank) > .75:
+            player_other['score'] = 0
+            return player_other
+
+    #add skill difference to score
+    skill_score += 1 - (skill_diff(target_tier, target_rank, other_tier, other_rank) / 2.0)
+    score += skill_score * skill_modifier
+    #role preference
+    role_score = player_target['playerGameRole']['partnerRole'] == player_other['playerGameRoll']['role']
+    score += role_score * role_modifier
+    #likeability score
+    like_score = player_other['likes']
+    score += like_score * like_modifier
+
+    player_other['score'] = score
     return player_other
 
+def skill_diff(this_tier, this_rank, other_tier, other_rank):
+    if this_tier == '':
+        this_rank = 5
+    else:
+        this_rank = int(this_rank)
+    if other_tier == '':
+        other_rank = 5
+    else:
+        other_rank = int(other_rank)
 
-
+    return abs((this_tier + (1 - this_rank *.2)) - (other_tier + (1 - other_rank *.2)))
